fb048e61a866e71e2c762f3534da3f25f58a1e00
Tawanna Hakkinen <thol_16@hotmail.com>
2023-10-18 23:26:46 -0300

    Contact class created

---
+++ lib/models/contact.dart
@@ -0,0 +1,81 @@
+class Contact {
+  String? objectId;
+  String name;
+  DateTime? birthDate;
+  String photo;
+  String phone;
+  String? place;
+  String? number;
+  String? neighborhood;
+  String? city;
+  String? state;
+
+  Contact({
+    required this.name,
+    required this.photo,
+    required this.phone,
+    this.objectId,
+    this.birthDate,
+    this.place,
+    this.number,
+    this.neighborhood,
+    this.city,
+    this.state,
+  });
+
+  factory Contact.empty() => Contact(name: '', photo: '', phone: '');
+
+  factory Contact.fromJson(Map<String, dynamic> json) {
+    return Contact(
+        objectId: json['objectId'],
+        name: json['name'],
+        photo: json['photo'],
+        phone: json['phone'],
+        birthDate: json['birthDate'] != null
+            ? DateTime.parse(json['birthDate']).toLocal()
+            : json['birthDate'],
+        place: json['place'],
+        number: json['number'],
+        neighborhood: json['neighborhood'],
+        city: json['city'],
+        state: json['state']);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'nome': name,
+        'telefone': phone,
+        'dataNascimento': {'__type': 'Date', 'iso': '$birthDate'},
+        'foto': photo,
+        'logradouro': place,
+        'numero': number,
+        'bairro': neighborhood,
+        'cidade': city,
+        'estado': state
+      };
+
+  Contact copyWith({
+    String? objectId,
+    String? name,
+    String? phone,
+    String? photo,
+    DateTime? birthDate,
+    String? place,
+    String? number,
+    String? neighborhood,
+    String? city,
+    String? state,
+  }) {
+    return Contact(
+      objectId: objectId ?? this.objectId,
+      name: name ?? this.name,
+      photo: photo ?? this.photo,
+      phone: phone ?? this.phone,
+      birthDate: birthDate ?? this.birthDate,
+      place: place ?? this.place,
+      number: number ?? this.number,
+      neighborhood: neighborhood ?? this.neighborhood,
+      city: city ?? this.city,
+      state: state ?? this.state,
+    );
+  }
+}
