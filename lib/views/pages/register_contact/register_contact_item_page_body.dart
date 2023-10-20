ae92cc003c2d691e97d47e177f6587534cf30ff4
Tawanna Hakkinen <thol_16@hotmail.com>
2023-10-19 21:20:27 -0300

    RegisterContactItemPageBody widget created

---
+++ lib/views/pages/register_contact/register_contact_item_page_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegisterContactItemPageBody extends StatelessWidget {
+  const RegisterContactItemPageBody({super.key, required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(24),
+      child: Form(
+        child: Column(
+          children: children,
+        ),
+      ),
+    );
+  }
+}
