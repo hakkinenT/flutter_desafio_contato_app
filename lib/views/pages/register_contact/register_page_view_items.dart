0c331693c65a901ef65d27c9f2404c8c664dcb1b
Tawanna Hakkinen <thol_16@hotmail.com>
2023-10-19 21:24:01 -0300

    Register page view items created

---
+++ lib/views/pages/register_contact/register_page_view_items.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_desafio_contato_app/views/pages/register_contact/address_data.dart';
+import 'package:flutter_desafio_contato_app/views/pages/register_contact/personal_data_page.dart';
+import 'package:flutter_desafio_contato_app/views/pages/register_contact/photo_data.dart';
+
+const List<Widget> pages = [
+  PersonalDataPage(),
+  AddressData(),
+  PhotoData(),
+];
