f8313680286166a1e1153f1021501a91fd161407
Tawanna Hakkinen <thol_16@hotmail.com>
2023-10-19 21:15:07 -0300

    FinishButton widget created

---
+++ lib/views/widgets/finish_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FinishButton extends StatelessWidget {
+  const FinishButton({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: onPressed, child: const FinishLabel());
+  }
+}
+
+class FinishLabel extends StatelessWidget {
+  const FinishLabel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          'Finalizar',
+          style: TextStyle(fontSize: 18),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Icon(Icons.check),
+      ],
+    );
+  }
+}
