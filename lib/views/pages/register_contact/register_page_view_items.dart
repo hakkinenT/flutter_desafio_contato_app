77f27055df4867013583fb06ad9a5abc16d28cc8
Tawanna Hakkinen <thol_16@hotmail.com>
2023-10-21 09:58:53 -0300

    RegisterPageViewItems fixed imports

--- lib/views/pages/register_contact/register_page_view_items.dart
+++ lib/views/pages/register_contact/register_page_view_items.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desafio_contato_app/views/pages/register_contact/address_data.dart';
-import 'package:flutter_desafio_contato_app/views/pages/register_contact/personal_data_page.dart';
-import 'package:flutter_desafio_contato_app/views/pages/register_contact/photo_data.dart';
+
+import 'address_data.dart';
+import 'personal_data_page.dart';
+import 'photo_data.dart';
 
 const List<Widget> pages = [
   PersonalDataPage(),
